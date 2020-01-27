5cba510154ee81b19035027a0c071525bdcf9817
Adrian Wnuk <adrwnuk@gmail.com>
2020-01-27 18:21:03 +0100

    Updates

---
+++ precache-manifest.cf1f51b111c53c0d2d8bfe6fa9d4ffcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8003698cfb9909a3bd9acd82965e1c76",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "eabd8555bbc3f0596059",
+    "url": "/monsters-rolodex/static/css/main.c14e90c4.chunk.css"
+  },
+  {
+    "revision": "5f64445c3b396716b777",
+    "url": "/monsters-rolodex/static/js/2.7e82c9cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7e82c9cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eabd8555bbc3f0596059",
+    "url": "/monsters-rolodex/static/js/main.5d252713.chunk.js"
+  },
+  {
+    "revision": "8e0631e8fb00ca5a6969",
+    "url": "/monsters-rolodex/static/js/runtime-main.41315a6c.js"
+  }
+]);
